0fd6945bc2dde42ebfb821fdd10a62ddcfd947af
HasanKarilov <hkarilov@gmail.com>
2017-05-06 01:29:50 +0600

    Addin variable in JSP

--- src/main/java/com/dev/servlet/MainServlet.java
+++ src/main/java/com/dev/servlet/MainServlet.java
@@ -13,6 +13,7 @@ import java.io.PrintWriter;
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("name", "Developer");
         req.getRequestDispatcher("mypage.jsp").forward(req, resp);
     }
 

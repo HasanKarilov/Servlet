15e515f790f801c48ba8b96d6a99eac3ee06c43b
HasanKarilov <hkarilov@gmail.com>
2017-05-06 01:05:35 +0600

    HTTPServlet methods overriding

---
+++ src/main/java/com/dev/servlet/MainServlet.java
@@ -0,0 +1,24 @@
+package com.dev.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by hanaria on 5/6/17.
+ */
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello Servlet</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}

210cf91cca6231f31eceb24119167a38b533c570
HasanKarilov <hkarilov@gmail.com>
2017-05-06 01:32:45 +0600

    Using servlet for mapping instead of xml

--- src/main/java/com/dev/servlet/MainServlet.java
+++ src/main/java/com/dev/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package com.dev.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import java.io.PrintWriter;
 /**
  * Created by hanaria on 5/6/17.
  */
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
